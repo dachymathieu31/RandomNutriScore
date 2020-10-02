bc09ca0a8bacb02cb64cc68599cfeae7643bac03
Dachy Mathieu <mathieu.dachy@outlook.fr>
2020-10-02 14:01:09 +0200

    Update view as ImageSlider

--- src/view/RndNutriScoreView.java
+++ src/view/RndNutriScoreView.java
@@ -1,44 +1,41 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 
-import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JSlider;
-import javax.swing.SwingConstants;
+import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
 import api.view.IRndNutriScoreView;
 
-public class RndNutriScoreView implements IRndNutriScoreView {
-	private JFrame frame;
-	private JSlider slider;
-	private GroupLayout layout;
+public class RndNutriScoreView extends JFrame implements IRndNutriScoreView {
+	private ImageIcon images[];
+	private JLabel imageLabel;
 
 	public RndNutriScoreView() {
 		SwingUtilities.invokeLater(() -> {
-			this.frame = new JFrame("NutriScore");
-			this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			Dimension dim = new Dimension(200, 100);
-			this.frame.setPreferredSize(dim);
-			this.frame.setVisible(true);
+			this.setLayout(new BorderLayout());
+			this.setTitle("NutriScore");
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.setSize(new Dimension(750, 450));
+			this.setVisible(true);
+			this.imageLabel = new JLabel("", JLabel.CENTER);
+			this.add(this.imageLabel, BorderLayout.CENTER);
 
-			this.slider = new JSlider(SwingConstants.HORIZONTAL, 0, 4, 0);
-			this.layout = new GroupLayout(this.frame.getContentPane());
-			this.layout.setAutoCreateGaps(true);
-			this.layout.setAutoCreateContainerGaps(true);
-			this.layout.setVerticalGroup(this.layout.createSequentialGroup().addGroup(
-					this.layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(this.slider)));
+			images = new ImageIcon[5];
 
-			this.frame.pack();
-			this.frame.setVisible(true);
+			images[0] = new ImageIcon("images/a.jpg");
+			images[1] = new ImageIcon("images/b.jpg");
+			images[2] = new ImageIcon("images/c.jpg");
+			images[3] = new ImageIcon("images/d.jpg");
+			images[4] = new ImageIcon("images/e.jpg");
 		});
 	}
 
 	@Override
-	public void updateView(int rndInt) {
-		if (this.slider.isEnabled()) {
-			this.slider.setValue(rndInt);
-		}
+	public void updateView(int imageIndex) {
+		this.imageLabel.setIcon(images[imageIndex]);
 	}
 }
