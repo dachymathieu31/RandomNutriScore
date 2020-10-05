72eb4c8cfdff706a9529af6fc41f0e9f0156cbdc
Dachy Mathieu <mathieu.dachy@outlook.fr>
2020-10-05 13:07:45 +0200

    Update Nutriscore view

--- src/impl/NutriScoreController.java
+++ src/impl/NutriScoreController.java
@@ -4,23 +4,22 @@ import java.util.TimerTask;
 
 import api.controller.IRandCaller;
 import api.view.IRndNutriScoreView;
-import view.RndNutriScoreView2;
+import view.RndNutriScoreView;
 
 public class NutriScoreController extends TimerTask {
 
 	private IRndNutriScoreView rndNutriView;
+	private IRandCaller rndCaller;
 
 	public NutriScoreController() {
-//		this.rndNutriView = new RndNutriScoreView();
-		this.rndNutriView = new RndNutriScoreView2();
+		this.rndNutriView = new RndNutriScoreView();
+		this.rndCaller = new RandCaller("https://api.random.org/json-rpc/2/invoke",
+				"1ddb11f7-ab2d-4cb8-a366-68c13d63951b");
 	}
 
 	@Override
 	public void run() {
-		IRandCaller rndCaller = new RandCaller("https://api.random.org/json-rpc/2/invoke",
-				"1ddb11f7-ab2d-4cb8-a366-68c13d63951b");
-		int rndInt = rndCaller.generateOneRndInt(0, 4, true);
+		int rndInt = this.rndCaller.generateOneRndInt(0, 4, true);
 		this.rndNutriView.updateView(rndInt);
-		System.out.println(rndInt);
 	}
 }
--- src/view/RndNutriScoreView.java
+++ src/view/RndNutriScoreView.java
@@ -3,15 +3,21 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.border.Border;
 
 import api.view.IRndNutriScoreView;
 
-public class RndNutriScoreView2 extends JFrame implements IRndNutriScoreView {
+public class RndNutriScoreView extends JFrame implements IRndNutriScoreView {
+
+	private static Dimension DEFAULT_DIM = new Dimension(50, 50); // Default dimension for each Letter label
+	private static Dimension FOCUS_DIM = new Dimension(60, 60); // Dimension for the focused letter
 
 	private JLabel a;
 	private JLabel b;
@@ -19,13 +25,25 @@ public class RndNutriScoreView2 extends JFrame implements IRndNutriScoreView {
 	private JLabel d;
 	private JLabel e;
 
-	public RndNutriScoreView2() {
+	private int selectedLetter;
+
+	public RndNutriScoreView() {
+		this.selectedLetter = Integer.MAX_VALUE;
 		SwingUtilities.invokeLater(() -> {
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			FlowLayout nutriFlow = new FlowLayout();
+			nutriFlow.setVgap(-30);
 			JLabel nutriLabel = new JLabel("Nutri-Score");
+			nutriLabel.setLayout(nutriFlow);
 			nutriLabel.setHorizontalAlignment(JLabel.CENTER);
+
 			JPanel nutriSlider = new JPanel();
+			FlowLayout sliderFlow = new FlowLayout();
+			sliderFlow.setVgap(45);
+			nutriSlider.setLayout(sliderFlow);
+
 			this.add(nutriLabel, BorderLayout.NORTH);
-			this.add(nutriSlider, BorderLayout.CENTER);
+			this.add(nutriSlider, BorderLayout.SOUTH);
 			this.a = createLabel("A", new Color(9.0f / 255f, 139.0f / 255f, 49.0f / 255f));
 			this.b = createLabel("B", new Color(153.0f / 255f, 196.0f / 255f, 46.0f / 255f));
 			this.c = createLabel("C", new Color(251.0f / 255f, 186.0f / 255f, 0.0f / 255f));
@@ -38,23 +56,64 @@ public class RndNutriScoreView2 extends JFrame implements IRndNutriScoreView {
 			nutriSlider.add(e);
 
 			this.setSize(400, 200);
+			this.setResizable(false);
 			this.setVisible(true);
 		});
 	}
 
 	private JLabel createLabel(String text, Color color) {
 		JLabel label = new JLabel(text);
+		Border whiteLine = BorderFactory.createLineBorder(Color.WHITE, 5);
+		label.setBorder(whiteLine);
 		label.setForeground(Color.WHITE);
 		label.setBackground(color);
 		label.setOpaque(true);
 		label.setHorizontalAlignment(JLabel.CENTER);
 		label.setVerticalAlignment(JLabel.CENTER);
-		label.setPreferredSize(new Dimension(50, 50));
+		label.setPreferredSize(DEFAULT_DIM);
 		return label;
 	}
 
 	@Override
 	public void updateView(int rndInt) {
+		if (selectedLetter != rndInt) {
+			selectedLetter = rndInt;
 
+			SwingUtilities.invokeLater(() -> {
+				// Reset all nutriSlider
+				a.setSize(DEFAULT_DIM);
+				b.setSize(DEFAULT_DIM);
+				c.setSize(DEFAULT_DIM);
+				d.setSize(DEFAULT_DIM);
+				e.setSize(DEFAULT_DIM);
+
+				// Choose the letter to focus
+				switch (selectedLetter) {
+				case 0: {
+					a.setSize(FOCUS_DIM);
+					break;
+				}
+				case 1: {
+					b.setSize(FOCUS_DIM);
+					break;
+				}
+				case 2: {
+					c.setSize(FOCUS_DIM);
+					break;
+				}
+				case 3: {
+					d.setSize(FOCUS_DIM);
+					break;
+				}
+				case 4: {
+					e.setSize(FOCUS_DIM);
+					break;
+				}
+				default: {
+					throw new IllegalArgumentException("Unexpected value: " + selectedLetter);
+				}
+				}
+			});
+		}
 	}
 }
