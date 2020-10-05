600686883a83ec8531a7dd01d3eda25ec31aae2b
Dachy Mathieu <mathieu.dachy@outlook.fr>
2020-10-05 14:41:49 +0200

    Remove config file

--- src/impl/NutriScoreController.java
+++ src/impl/NutriScoreController.java
@@ -1,9 +1,6 @@
 package impl;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 import java.util.TimerTask;
 
 import api.controller.IRandCaller;
@@ -17,16 +14,8 @@ public class NutriScoreController extends TimerTask {
 
 	public NutriScoreController() throws IOException {
 		this.rndNutriView = new RndNutriScoreView();
-		Properties props = new Properties();
-		String propFileName = "config.properties";
-		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(propFileName);
-		if (inputStream != null) {
-			props.load(inputStream);
-		} else {
-			throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-		}
-
-		this.rndCaller = new RandCaller("https://api.random.org/json-rpc/2/invoke", props.getProperty("apikey"));
+		this.rndCaller = new RandCaller("https://api.random.org/json-rpc/2/invoke",
+				"1ddb11f7-ab2d-4cb8-a366-68c13d63951b");
 	}
 
 	@Override
